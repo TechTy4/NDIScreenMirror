e78dd3f11ebc8d91e30e2684030c3b19aaa69554
Elijah Landis <TechnicalTy@icloud.com>
2026-09-02 17:53:04 -0400

    Initial Commit

---
+++ NDIScreenMirror/NDIScreenMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDIScreenMirrorApp.swift
+//  NDIScreenMirror
+//
+//  Created by Elijah Landis on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct NDIScreenMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
